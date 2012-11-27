515ec04bb515ef062d9b198f023e53c8772e34a7
Professional Mole <professional.mole@yahoo.com.br>
2012-11-27 17:12:14 -0200

    Random Library to make all my randomizations easier
    Gotta document this later

---
+++ src/common/Mole/common/RandomUtil.java
@@ -0,0 +1,84 @@
+package Mole.common;
+
+class RandomUtil 
+{
+	public static class ODD
+	{
+		int oddVal;
+		Object result;
+		
+		public ODD(int o, Object O)
+		{
+			oddVal = o;
+			result = O;
+		}
+		
+		public ODD(Object O)
+		{
+			if (O instanceof Object[])
+			{
+				Object[] _O = (Object[]) O;
+				switch(_O.length)
+				{
+					case 0:
+						oddVal = 0;
+						result = null;
+						break;
+					case 1:
+						oddVal = 1;
+						result = _O[0];
+						break;
+					default:
+						if (_O[0] instanceof Integer)
+						{
+							oddVal = (Integer) _O[0];
+							result = _O[1];
+						}
+						else
+						{
+							oddVal = 1;
+							result = _O;
+						}
+						break;
+				}
+			}
+			else
+			{
+				oddVal = 1;
+				result = O;
+			}
+		}
+		
+		public int getOdds() { return oddVal; }
+		public Object get() { return result; }
+	}
+	
+	public static Object randomize(ODD[] odds, Random rand)
+	{
+		int totalOdds = 0;
+		for (ODD o: odds)
+		{
+			totalOdds += o.getOdds();
+		}
+		
+		int baseChance = rand.nextInt(totalOdds);
+		for (ODD o: odds)
+		{
+			if (baseChance < o.getOdds()) return o.get();
+			baseChance -= o.getOdds();
+		}
+		
+		return null;
+	}
+	
+	public static Object randomize(Object[] odds, Random rand)
+	{
+		ODD[] O = new ODD[odds.length];
+		for (int i=0; i < odds.length; i++)
+		{
+			O[i] = new ODD(odds[i]);
+		}
+		
+		return randomize(O, rand);
+	}
+}

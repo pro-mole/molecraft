37560a610a80f2ab628f694a6bae02cd90b12901
Bruno Guedes <professional.mole@yahoo.com.br>
2012-12-12 20:42:09 -0200

    Seedstone now nigh-indestructible and not dropping

--- src/common/Mole/common/block/BlockSeedstone.java
+++ src/common/Mole/common/block/BlockSeedstone.java
@@ -1,12 +1,12 @@
 package Mole.common.block;
 
 import net.minecraft.src.BlockContainer;
+import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.Material;
 import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
 import Mole.common.Constants;
 import Mole.common.item.Seedstone.EnumSeedstoneType;
-import Mole.common.seedstone.TileSeedstoneHouse;
 
 public class BlockSeedstone extends BlockContainer {
 	
@@ -16,6 +16,8 @@ public class BlockSeedstone extends BlockContainer {
 	{
 		super(Constants.MOLE_BLOCK_SEEDSTONE, Material.ground);
 		this.setLightValue(0.8F);
+		this.setHardness(50);
+		this.setResistance(2000);
 	}
 	
 	@Override
@@ -33,5 +35,11 @@ public class BlockSeedstone extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World var1) {
 		return null;
+	}
+	
+	@Override
+	public boolean canHarvestBlock(EntityPlayer player, int metadata)
+	{
+		return false;
 	}
 }
